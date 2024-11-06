3cc326d2c16afd80b05ac5bb55168fbf550ca941
Chandan <sahoobipin2000@gmail.com>
2024-11-07 01:27:10 +0530

    changes in string file

--- src/com/bbs/string/StringSubString.java
+++ src/com/bbs/string/StringSubString.java
@@ -3,7 +3,7 @@ package com.bbs.string;
 public class StringSubString {
 	public static void main(String[] args) {
 		String s1 = "bipin";
-		String substring = s1.substring(1, 3);
+		String substring = s1.substring(0, 2);
 		System.out.println(substring);
 	}
 
